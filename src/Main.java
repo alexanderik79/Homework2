71b282aa74533e882a7fa4fd6840b1e5a8f824f0
alexanderik79 <alexanderik79@gmail.com>
2023-06-03 09:44:58 +0200

    1234567890

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("test github");
         System.out.println("Simenko_git");
         System.out.println("wwww");
+        System.out.println("1234567890");
     }
 }
